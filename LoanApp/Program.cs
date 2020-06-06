49d11cc4d22b4a25a6cd311c0189c37d83288f96
PresidentCourtnee <52513651+PresidentCourtnee@users.noreply.github.com>
2020-06-06 17:07:02 -0600

    Final
    This is the finalized project

--- LoanApp/Program.cs
+++ LoanApp/Program.cs
@@ -10,6 +10,29 @@ namespace LoanApp
     {
         static void Main(string[] args)
         {
+            LoanInfo info = new LoanInfo();
+            info.CustName = "Courtnee & Emi";
+            info.LoanNum = "456789123";
+            info.LoanAmt = "452000";
+
+            Console.WriteLine(info);
+
+            AutoLoan auto = new AutoLoan();
+            auto.Make = "Pathfinder";
+            auto.Model = "Nissan";
+            auto.Year = "2013";
+            auto.VIN = "12345678912345";
+            auto.CarAmt = "32000";
+
+            Console.WriteLine(auto);
+
+            HomeLoan home = new HomeLoan();
+            home.TypeOfConstruct = "Stucco";
+            home.YrBuilt = "2013";
+            home.SqFt = "3600";
+            home.HomeAmt = "420000";
+
+            Console.WriteLine(home);
         }
     }
 }
